d61496928fe1a0a0eac6ea78d1257af6fe858131
Ole Kristian Kåseth <oklk@gapit.io>
2020-09-29 11:31:26 +0200

    Initial commit

---
+++ src/types/code-data.d.ts
@@ -0,0 +1,4 @@
+/**
+ * The codeData as a dictionary.
+ */
+declare const codeData: { [key: string]: any };
---
+++ src/types/window.d.ts
@@ -0,0 +1,8 @@
+interface Window {
+  svgData: string;
+  htmlNode: typeof htmlNode;
+  codeData: typeof codeData;
+  data: typeof data;
+  options: typeof options;
+  theme: typeof theme;
+}
